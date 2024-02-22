1526338d484acff93bb049e2e4610ff0df6a270d
neallseth <32910906+neallseth@users.noreply.github.com>
2024-02-21 23:08:17 -0800

    Initial Commit

---
+++ guardian/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guardian
+//
+//  Created by Neall Seth on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guardian/guardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  guardianApp.swift
+//  guardian
+//
+//  Created by Neall Seth on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct guardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
